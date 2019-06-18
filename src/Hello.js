46c6b124ed213a4a094636f4fc014377613f9f01
Jerry Ferrer <jerryferrer@fpcph-mbp.local>
2019-06-18 10:52:19 +0800

    Sample react component - Hello

--- src/Hello.js
+++ src/Hello.js
@@ -5,9 +5,9 @@ class Hello extends Component {
 
   render() {
     return (
-      <div>
+      <div className='f1 tc'>
         <h1> Hello, World!</h1>
-        <p>Welcome to React</p>
+        <p>{this.props.greeting}</p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 //ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'Hello ' + 'React Ninja'}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
